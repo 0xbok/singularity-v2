dd4f2d3d58a9af68771ac5fcd571ecfbeedde08c
0xbok <1689531+0xbok@users.noreply.github.com>
2022-06-15 03:07:59 +0530

    foundry setup

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-import {Contract} from "src/Contract.sol";
+import {SingularityFactory} from "contracts/SingularityFactory.sol";
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
-        new Contract();
+        new SingularityFactory("a", address(1), address(1), address(1));
     }
 }
